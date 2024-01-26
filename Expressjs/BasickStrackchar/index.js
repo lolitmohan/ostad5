c54c9d1d21695370bf0ad5f8e1604289826bed9c
lolitmohan <lolitmohan090@gmail.com>
2024-01-26 22:47:15 +0600

    Add server

--- Expressjs/BasickStrackchar/index.js
+++ Expressjs/BasickStrackchar/index.js
@@ -2,3 +2,15 @@ const app=require('./app');
 app.listen(5050, function(){
     console.log("Server Run Success");
 })
+
+
+
+
+/*const express=require('express');
+app=express();
+app.get("/",function(req,res){
+    res.send("Hello Express js");
+})
+app.listen(4000, function(){
+    console.log("Server Run Success 4000 port")
+})*/
